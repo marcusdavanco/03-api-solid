321e58569f035b93e22a6fe066dca8951c4ab4f2
Marcus Davanço <md.daves@gmail.com>
2023-08-24 09:53:03 -0300

    feat: Factories dos casos de uso

---
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -0,0 +1,9 @@
+import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-check-ins-history'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prima-check-in-repository'
+
+export function makeFetchUserCheckInsHistoryUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const useCase = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+
+  return useCase
+}
