8b2dec91a30ba2495df161ea022b01a6d856529a
Marcus Davanço <md.daves@gmail.com>
2023-08-24 10:36:44 -0300

    feat: Princípios da autenticação JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', register)
+
+  // Authenticated
+  app.get('/me', profile)
 }
