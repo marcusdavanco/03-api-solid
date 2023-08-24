5473a76a01de839363b905305b65798e53ccab50
Marcus Davanço <md.daves@gmail.com>
2023-08-24 11:05:27 -0300

    feat: Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', register)
 
   // Authenticated
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
