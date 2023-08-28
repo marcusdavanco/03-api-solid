3a27377cd7dd0f065fdadaf20135f1dd276ed338
Marcus Davanço <md.daves@gmail.com>
2023-08-28 09:54:27 -0300

    feat: Teste E2E da autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,10 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { profile } from './controllers/profile'
 import { verifyJWT } from './middlewares/verify-jwt'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-  app.post('/sessions', register)
+  app.post('/sessions', authenticate)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJWT] }, profile)
